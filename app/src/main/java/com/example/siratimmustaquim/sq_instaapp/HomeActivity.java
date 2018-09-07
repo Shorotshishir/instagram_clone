068b158b59a742825ec282cc06b1e8ae013dc1b6
Shorotshishir <tanzim121@gmail.com>
2018-09-07 20:49:22 +0600

    added bottom layer icons, and menu

--- app/src/main/java/com/example/siratimmustaquim/sq_instaapp/HomeActivity.java
+++ app/src/main/java/com/example/siratimmustaquim/sq_instaapp/HomeActivity.java
@@ -2,12 +2,29 @@ package com.example.siratimmustaquim.sq_instaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.siratimmustaquim.sq_instaapp.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private static final String TAG = "HomeActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: Starting. . .");
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView Setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up bottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        bottomNavigationViewEx.enableAnimation(false);
     }
 }
---
+++ app/src/main/java/com/example/siratimmustaquim/sq_instaapp/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,16 @@
+package com.example.siratimmustaquim.sq_instaapp.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+    public static void  setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
